cd05c003fe5c14eb969e0c1e0c92a7c3d125058c
Radoslaw Kuschel <radoslaw.kuschel@gmail.com>
2016-11-26 18:56:45 +0000

    RTC date/time and timestamp in Unix format

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 # FinalYearProject
 
 Branch DHT11: Rceive data from DHT 11 sensor. Display Humidity and Temperature every second in command window.
+New commit in master. Adding RTC diplaying date/time and timestamp in Unix format.
--- Sensors.py
+++ Sensors.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
 import sys
 import Adafruit_DHT
+import time
+import datetime 
+
 
 while True:
 
+    now = time.strftime("%c")
+    dtime = datetime.datetime.now()
+    timestamp = time.mktime(dtime.timetuple())
+    currentTime = ("Current time is %s" % now)
+
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    time.sleep(1)
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} % '.format(temperature, humidity) + currentTime 
+    print  " Unix time is: " + str(timestamp)
+    #print ("Current time is %s" % now)
+
